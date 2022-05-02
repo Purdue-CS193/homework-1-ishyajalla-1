bc1a276d0cf29d88d8fff24921e81cbbcd209a5f
ishyajalla <90941032+ishyajalla@users.noreply.github.com>
2022-05-02 06:04:41 -0400

    Update answers.sh

--- answers.sh
+++ answers.sh
@@ -4,42 +4,44 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm -rf Memes/Spongebob/money.jpeg
 
 # Task 3
-
+mv Memes/StarWars/fafsa.jpeg Memes/Spongebob//fafsa.jpe
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++
 
 # Task 7
-
+cp Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/*.c Programs/C
 
 # Task 9
-
+mv Programs/*.py Programs/Python3
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls
 
 # Task 12
 : '
 Type your vim/nano customizations below:
-
+set nowrap
+set number
+set showmatch
 
 
 '
